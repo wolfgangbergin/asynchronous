dbb982adbb54e5097be22656559c77f36f0a54c6
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-08-21 21:12:51 -0400

    temp

--- javascript/test8.js
+++ javascript/test8.js
@@ -1,11 +1,11 @@
 
-l('test start')
-setTimeout(()=>{l('0 sec timer')},0)
-Promise.resolve('Resolved promise 1').then(res=>l(res))
-Promise.resolve('Resolved promise 2').then(res=>{for(let i=0;i<3000000000;i++){};l(res)})
+// l('test start')
+// setTimeout(()=>{l('0 sec timer')},0)
+// Promise.resolve('Resolved promise 1').then(res=>l(res))
+// Promise.resolve('Resolved promise 2').then(res=>{for(let i=0;i<3000000000;i++){};l(res)})
 
-Promise.resolve('Resolved promise 1').then(res=>l(res))
-l('test end')
+// Promise.resolve('Resolved promise 1').then(res=>l(res))
+// l('test end')
 
 
 
