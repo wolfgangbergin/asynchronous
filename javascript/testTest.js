abc04d913b1b2f6c2d234fdc26be126efc9b0442
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-09-01 11:57:50 -0400

    temp

--- javascript/testTest.js
+++ javascript/testTest.js
@@ -4,6 +4,45 @@
 
 
 
+// wolfgang.wolffunc = function (country) {
+//     wolfgang
+//       .wolfJson(
+//         country,
+//         undefined,
+//         wolfgang.renderCountry,
+//         `country Not Found`,
+//         `https://restcountries.com/v2/name`
+//       )
+//       .then(response => {
+//         if (!response.borders) {
+//           throw new Error(`${response.name} Has no Borders`);
+//         }
+//         return wolfgang.wolfJson(
+//           response?.borders[1],
+//           'neighbour',
+//           wolfgang.renderCountry,
+//           `country Not Found`,
+//           `https://restcountries.com/v2/alpha`
+//         );
+//       })
+//       .then(response => {
+//         if (!response.borders) {
+//           throw new Error(`${response.name} Has no Borders`);
+//         }
+//         return wolfgang.wolfJson(
+//           response.borders[1],
+//           'neighbour',
+//           wolfgang.renderCountry,
+//           `country Not Found`,
+//           `https://restcountries.com/v2/alpha`
+//         );
+//       })
+//       .catch(error => {
+//         d(error);
+//         wolfgang.renderError(`${error.message}🇦🇱🇦🇱🇦🇱`);
+//       })
+//       .finally(() => (wolfgang.countriesContainer.style.opacity = 1));
+//   };
 
 
 
